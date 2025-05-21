a1b492ee57b8f37b78edcf08510c0c5e057594a4
Vin-cen-zo <vincenzo.tisila@gmail.com>
2025-05-21 18:26:10 +0700

    Merapikan kode

--- main.c
+++ main.c
@@ -5,43 +5,42 @@ typedef struct {
 	char nama[50];
 	float daya;
 	float durasi;
-} data_perangkat;
+} Perangkat;
 
 int main (){
 	int input; //yang diinput user nanti untuk memilih apakah mau lanjut menambahkan perangkat atau tidak
 
-	data_perangkat *perangkat = NULL; //buat pointer ke struct 
-	perangkat= (data_perangkat*)malloc(1*sizeof(data_perangkat)); //alokasi ukuran array perangkat jadi 1 terlebih dahulu
-	int i=0; //counter
+	Perangkat *perangkat = (Perangkat *)malloc(1 * sizeof(Perangkat)); //buat pointer ke struct dan alokasi ukuran array perangkat jadi 1 terlebih dahulu
+	int i = 0; //counter
 
 	printf("=========================================\n");
 	printf("        Renewable Energy Simulator       \n");
 	printf("=========================================\n");
 	//Kenapa gak pake for loop? -Rasya
-	do{
-		printf("\nPERANGKAT-%d\n", i+1);
-		printf("Nama perangkat: ");
+	do {
+		printf("\nPerangkat Elektronik-%d\n", i+1);
+		printf("Nama perangkat elektronik: ");
 		getchar();
 		scanf(" %[^\n]", perangkat[i].nama);
 
 		printf("Daya (Watt): ");
 		scanf("%f", &perangkat[i].daya);
 
-		printf("Durasi pemakaian per hari (Jam): ");
+		printf("Rata-rata durasi pemakaian per hari (jam): ");
 		scanf("%f", &perangkat[i].durasi);
 
 		i++; 
 
-		printf("\nApakah Anda ingin menambahkan perangkat lagi? (1= Ya, 0= Tidak): ");
+		printf("\nApakah Anda ingin menambahkan perangkat lagi? (1 = Ya, 0 = Tidak): ");
 		scanf("%d", &input);
-		if (input==0){
+		if (input == 0) {
 			break;
 		}
 		//jika masih lanjut, maka tambah ukuran array (+ 1)
-  		perangkat= (data_perangkat*) realloc (perangkat, (i+1)*sizeof(data_perangkat));
-	} while(input == 1);
+  		perangkat = (Perangkat *) realloc (perangkat, 1 * sizeof(Perangkat));
+	} while (input == 1);
 
-	int pick, jml;//Pilihan Renewable Energy yang mana, Jumlah alat renewable energy
+	int pick, jml; //Pilihan Renewable Energy yang mana, Jumlah alat renewable energy
 	int initialCost, energy; //Initial Cost dan Energy
 	
 	printf("Pilih Renewable Energy yang ingin digunakan :\n1. Panel Surya\n2. Kincir Angin\n3. Kincir Air\n : ");
