3720a81896c82f1724f3aa45b4b1e83013287390
Vin-cen-zo <vincenzo.tisila@gmail.com>
2025-05-26 22:46:46 +0700

    Menambahkan logika kode

--- main.c
+++ main.c
@@ -29,45 +29,60 @@ void install_turbin_air (float harga_kwh, float biaya_listrik[], Alat turbin_air
 }
 
 int main (){
-	int pilihan, i = 0; //yang diinput user nanti untuk memilih apakah mau lanjut menambahkan perangkat atau tidak dan i = counter
+	int pilihan, n = 0; //yang diinput user nanti untuk memilih apakah mau lanjut menambahkan perangkat atau tidak dan i = counter
 	float harga_kwh = 1444.7;
+	float total_kwh[2] = {0};
+	float biaya_listrik[2] = {0};
 	Perangkat *perangkat = (Perangkat *)malloc(1 * sizeof(Perangkat)); //buat pointer ke struct dan alokasi ukuran array perangkat jadi 1 terlebih dahulu
 
 	printf("=========================================\n");
 	printf("        Renewable Energy Simulator       \n");
 	printf("=========================================\n");
 
-	do {
-		printf("\nPerangkat Elektronik-%d\n", i+1);
-		printf("Nama perangkat elektronik: ");
-		getchar();
-		scanf(" %[^\n]", perangkat[i].nama);
+	printf("\nPilih mode input simulator\n");
+	printf("1. Input daya per perangkat (jika tidak mengetahui listrik yang digunakan rumah per bulan)\n");
+	printf("2. Input daya total per bulan\n");
+	printf("Pilihan (1 atau 2) : ");
+	scanf("%d", &pilihan);
 
-		printf("Daya (Watt): ");
-		scanf("%f", &perangkat[i].daya_perangkat);
+	if (pilihan == 1) {
+		do {
+			printf("\nPerangkat Elektronik - %d\n", n + 1);
+			printf("Nama : ");
+			getchar();
+			scanf(" %[^\n]", perangkat[n].nama);
 
-		printf("Rata-rata durasi pemakaian per hari (jam): ");
-		scanf("%f", &perangkat[i].durasi_perangkat); 
+			printf("Daya (Watt): ");
+			scanf("%f", &perangkat[n].daya_perangkat);
 
-		printf("\nApakah Anda ingin menambahkan perangkat lagi? (1 = Ya, 0 = Tidak): ");
-		scanf("%d", &pilihan);
-		if (pilihan == 0) {
-			break;
-		}
-		//jika masih lanjut, maka tambah ukuran array (+ 1)
-  		perangkat = (Perangkat *) realloc (perangkat, (i + 2) * sizeof(Perangkat));
+			printf("Rata-rata durasi pemakaian per hari (jam): ");
+			scanf("%f", &perangkat[n].durasi_perangkat); 
 
-		i++;
-	} while (pilihan == 1);
+			printf("\nApakah masih ada perangkat lagi? (1 = Ya, 0 = Tidak): ");
+			scanf("%d", &pilihan);
+			if (pilihan == 0) {
+				break;
+			}
+			//jika masih lanjut, maka tambah ukuran array (+ 1)
+  			perangkat = (Perangkat *) realloc (perangkat, (n + 2) * sizeof(Perangkat));
+			n++;
+		} while (pilihan == 1);
 
-	float total_kwh[2] = {0};
-	float biaya_listrik[2] = {0};
+		for (int i = 0; i < n + 1; i++) {
+			total_kwh[0] += perangkat[i].daya_perangkat * perangkat[i].durasi_perangkat / 1000 * 30;
+		} // menghitung kWh per bulan
 
-	for (int j = 0; j < i + 1; j++) {
-		total_kwh[0] += perangkat[j].daya_perangkat * perangkat[j].durasi_perangkat / 1000 * 30;
-		biaya_listrik[0] += total_kwh[0] * harga_kwh;
-	} // menghitung biaya listrik tanpa menggunakan alat renewable energy
+	} else if (pilihan == 2) {
+		printf("Daya total yang digunakan per bulan (kWh) : ");
+		scanf("%f", &total_kwh[0]);
 
+	} else {
+		printf("Input tidak sesuai\n");
+		return 1;
+	}
+
+	biaya_listrik[0] += total_kwh[0] * harga_kwh;
+	total_kwh[1] = total_kwh[0];
 	biaya_listrik[1] = biaya_listrik[0];
 
 	Alat panel_surya = {
@@ -83,7 +98,7 @@ int main (){
 		.daya_alat = 400
 	};
 
-	printf("Alat Renewable Energy\n");
+	printf("\nAlat Renewable Energy\n");
 	printf("1. Panel Surya\n");
 	printf("Harga alat : Rp%d\n", panel_surya.harga_alat);
 	printf("Daya alat : %d W\n", panel_surya.daya_alat);
@@ -100,8 +115,6 @@ int main (){
 	scanf("%d", turbin_angin.jumlah_alat);
 	printf("Input berapa jumlah Turbin Air yang diinginkan : ");
 	scanf("%d", turbin_air.jumlah_alat);
-	
-	
 	
     return 0; //selesai
 }
